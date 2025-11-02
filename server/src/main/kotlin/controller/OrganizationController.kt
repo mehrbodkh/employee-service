f14bd7b227928a57f6f1688874533ca29c674361
mehrbod <mehrbod.kh@gmail.com>
2025-11-02 18:25:26 +0100

    Cleanup organization

--- server/src/main/kotlin/controller/OrganizationController.kt
+++ server/src/main/kotlin/controller/OrganizationController.kt
@@ -16,20 +16,20 @@ class OrganizationController(
     override fun Route.routes() = route("/org") {
 
         get("/root") {
-            val depth = call.queryParameters["depth"]?.toInt() ?: 1
+            val depth = (call.queryParameters["depth"]?.toInt() ?: 1).coerceIn(1..10)
             val result = organizationService.getRootsSubordinates(depth)
             call.respond(result)
         }
 
         get("/{id}/supervisors") {
-            val depth = call.queryParameters["depth"]?.toInt() ?: 1
-            val id = call.parameters["id"] ?: ""
+            val depth = (call.queryParameters["depth"]?.toInt() ?: 1).coerceIn(1..10)
+            val id = call.parameters["id"]
             val result = organizationService.getSupervisors(UUID.fromString(id), depth)
             call.respond(result)
         }
 
         get("/{id}/hierarchy") {
-            val depth = call.queryParameters["depth"]?.toInt() ?: 1
+            val depth = (call.queryParameters["depth"]?.toInt() ?: 1).coerceIn(1..10)
             val id = call.parameters["id"] ?: ""
             val result = organizationService.getSubordinates(UUID.fromString(id), depth)
             call.respond(result)
